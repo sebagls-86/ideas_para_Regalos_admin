26240fb17ff2dbd0e687340e919f10be2f2c00b0
sebagls-86 <sebastianglopezsosa@gmail.com>
2024-04-09 11:46:32 -0300

    auth0 redirect

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ import "../src/assets/css/App.css";
 
 const config = getConfig();
 
+const redirectUri = process.env.NODE_ENV === 'production' ? 'https://ideas-para-regalos-admin.vercel.app/admin/default' : 'http://localhost:3000/admin/default';
+
+console.log(redirectUri)
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
       domain={config.domain}
       clientId={config.clientId}
-      redirectUri={"https://ideas-para-regalos-admin.vercel.app/admin/default"}
+      redirectUri={redirectUri}
       audience={config.audience}
       scope="read:current_user update:current_user_metadata"
     >
